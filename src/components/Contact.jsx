3aa762b33d61ae5991cfb7c49016b137ad268955
Mohamed Waked <mwaked01@gmail.com>
2024-04-27 01:08:09 -0400

    style contact modal

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,11 @@
+import "../styles/Contact.scss"
 
 import Modal from '@mui/material/Modal';
 import Backdrop from '@mui/material/Backdrop';
 import Fade from '@mui/material/Fade';
 import Box from '@mui/material/Box';
+
+import { Form} from 'semantic-ui-react';
 
 const Contact = (props) => {
   const { openContact, setOpenContact } = props;
@@ -26,12 +29,14 @@ const Contact = (props) => {
       >
         <Fade in={openContact}>
           <Box>
+            <Form>
             <div id='email'>
               mwaked01@gmail.com
             </div>
             <div id='location'>
               Windsor, Ontario, Canada
             </div>
+            </Form>
           </Box>
         </Fade>
       </Modal>
