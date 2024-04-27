ef4db6eba188bc6505c92ac06af19bb34fc438df
Mohamed Waked <mwaked01@gmail.com>
2024-04-26 23:47:30 -0400

    add skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Skills from './components/Skills'
 
 import './App.css'
 
@@ -7,23 +8,23 @@ function App() {
 
   return (
     <>
-    <header>
-      <h1>Hi 👋</h1>
-      <h1>I'm Mohamed Waked</h1>
-      <Navbar/>
-    </header>
-    <section id="ABOUT">
-    <h2>ABOUT ME</h2>
-          <p>
-            Motivated professional who is eager to
-            transition into a career in Web Development.
-            Highly motivated self starter with a strong passion
-            for programming. Committed to continuous learning
-            and staying up-to-date with the latest industry trends.
-            Looking forward to embracing new challenges.
-          </p>
-    </section>
-    
+      <header>
+        <h1>Hi 👋</h1>
+        <h1>I'm Mohamed Waked</h1>
+        <Navbar />
+      </header>
+      <section id="ABOUT">
+        <h2>ABOUT ME</h2>
+        <p>
+          Motivated professional who is eager to
+          transition into a career in Web Development.
+          Highly motivated self starter with a strong passion
+          for programming. Committed to continuous learning
+          and staying up-to-date with the latest industry trends.
+          Looking forward to embracing new challenges.
+        </p>
+      </section>
+      <Skills />
     </>
   )
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,99 @@
+
+const Skills = (props) => {
+
+  return (
+    <section id="SKILLS" >
+      <div>
+        <div className='title'>
+          <h3>
+            Development
+          </h3>
+          <div className='sub-section'>
+            <div className="icon">
+              JavaScript
+            </div>
+            <div className="icon">
+              Python
+            </div>
+            <div className="icon">
+              Ruby on Rails
+            </div>
+            <div className="icon">
+              HTML5
+            </div>
+            <div className="icon">
+              CSS3
+            </div>
+            <div className="icon">
+              SASS
+            </div>
+          </div>
+        </div>
+
+        <div className='title'>
+          <h3>
+            Frameworks/ Libraries
+          </h3>
+          <div className='sub-section'>
+            <div className="icon">
+              React
+            </div>
+            <div className="icon">
+              Express
+            </div>
+            <div className="icon">
+              JQuery
+            </div>
+            <div className="icon">
+              Bootstrap
+            </div>
+            <div className="icon">
+              MaterialUI
+            </div>
+          </div>
+        </div>
+
+        <div className='title'>
+          <h3>
+            Testing
+          </h3>
+          <div className='sub-section'>
+            <div className="icon">
+              Cypress
+            </div>
+            <div className="icon">
+              Jest
+            </div>
+            <div className="icon">
+              Mocha
+            </div>
+            <div className="icon">
+              Chai
+            </div>
+          </div>
+        </div>
+        <div className='title'>
+          <h3>
+            SYSTEMS AND TOOLS
+          </h3>
+          <div className='sub-section'>
+            <div className="icon">
+              PostgreSQL
+            </div>
+            <div className="icon">
+              Visual Studio
+            </div>
+            <div className="icon">
+              Git
+            </div>
+            <div className="icon">
+              Figma
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
