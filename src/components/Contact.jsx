3560145e0cf6146e1bf6ffadbaf1addad8c5cbd6
Mohamed Waked <mwaked01@gmail.com>
2024-04-27 01:13:07 -0400

    create and style form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,15 +4,17 @@ import Modal from '@mui/material/Modal';
 import Backdrop from '@mui/material/Backdrop';
 import Fade from '@mui/material/Fade';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Email, LocationOn } from '@mui/icons-material';
 
-import { Form} from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 
 const Contact = (props) => {
   const { openContact, setOpenContact } = props;
 
   const handleCloseContact = () => setOpenContact(false);
   return (
-
     <section id="CONTACT" >
       <Modal
         aria-labelledby="transition-modal-title"
@@ -30,12 +32,47 @@ const Contact = (props) => {
         <Fade in={openContact}>
           <Box>
             <Form>
-            <div id='email'>
-              mwaked01@gmail.com
-            </div>
-            <div id='location'>
-              Windsor, Ontario, Canada
-            </div>
+              <TextField
+                className='field'
+                id="outlined-multiline-static"
+                label="Email"
+                name='from_email'
+                inputProps={{ style: { color: "white" } }}
+                required
+                focused
+                placeholder='example@mail.ca'
+              />
+              <TextField
+                className='field'
+                id="outlined-multiline-static"
+                label="Name"
+                name='from_name'
+                inputProps={{ style: { color: "white" } }}
+                required
+                focused
+                placeholder='Your Name'
+              />
+              <TextField
+                className='field'
+                id="outlined-multiline-static"
+                label="Message"
+                name='message'
+                multiline
+                inputProps={{ style: { color: "white" } }}
+                rows={4}
+                required
+                focused
+              />
+
+              <Button id='form-button' type='submit' variant="contained" color='success' >Submit</Button>
+
+              <div id='email'>
+                <Email className='contact-icon' />
+                mwaked01@gmail.com
+              </div>
+              <div id='location'>
+                <LocationOn className='contact-icon' /> Windsor, Ontario, Canada
+              </div>
             </Form>
           </Box>
         </Fade>
